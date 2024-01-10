79703eda88ee633833ef160a624fdaf703e51a19
BogibekDev <BogibekDev@gmail.com>
2024-01-10 18:29:44 +0500

    textfield added

--- app/src/main/java/uz/dataunion/datashop/screens/bottom/ProfileScreen.kt
+++ app/src/main/java/uz/dataunion/datashop/screens/bottom/ProfileScreen.kt
@@ -6,7 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,7 +21,9 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 fun ProfileScreen(
     navgator: DestinationsNavigator? = null,
 ) {
-
+    var username by remember {
+        mutableStateOf("")
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -28,6 +35,13 @@ fun ProfileScreen(
             verticalArrangement = Arrangement.Center
         ) {
             Text(text = "ProfileScreen")
+            //added this field
+            TextField(
+                value = username,
+                onValueChange = {
+                    username = it
+                }
+            )
         }
     }
 
